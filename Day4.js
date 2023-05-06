ae3ba562e342aea7fd59c36e02729c039065aa96
MundruLavanya <lavanyamundru15@gmail.com>
2023-05-06 07:22:13 +0530

    initial document

---
+++ Day4.js
@@ -0,0 +1,11 @@
+// Write 10k in one line and coders in another line
+var a="10000 coders";
+console.log("10000\n coders")
+
+// By using for loop for thr length
+
+var a="Lavanya"
+
+for(i=0;i<=a.length;i++){
+    console.log(i)
+}
